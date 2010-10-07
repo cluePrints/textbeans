1c41da6e27c13cf98bed10a4115eeaf3d768ea01
some.unique.login <some.unique.login@0e4c521a-48eb-3509-bcc3-8a98805872c0>
2010-10-07 09:54:10 +0000

    Initial import.

---
+++ trunk/src/main/java/net/sf/textbeans/internal/grammar/Compiler.java
@@ -0,0 +1,57 @@
+package net.sf.textbeans.internal.grammar;
+
+import java.io.IOException;
+import java.io.PushbackReader;
+import java.io.Reader;
+import java.util.Collection;
+import java.util.List;
+
+import net.sf.textbeans.grammar.analysis.DepthFirstAdapter;
+import net.sf.textbeans.grammar.lexer.LexerException;
+import net.sf.textbeans.grammar.node.ATokenDeclaration;
+import net.sf.textbeans.grammar.node.Start;
+import net.sf.textbeans.grammar.node.Token;
+import net.sf.textbeans.grammar.parser.ParserException;
+
+import com.google.common.collect.Lists;
+
+public class Compiler {
+	public Collection<Tokenizer> generateTokenizers(Reader grammarSource) {
+		try {
+			net.sf.textbeans.grammar.lexer.Lexer lexer = new net.sf.textbeans.grammar.lexer.Lexer(new PushbackReader(grammarSource));
+			net.sf.textbeans.grammar.parser.Parser p = new net.sf.textbeans.grammar.parser.Parser(lexer);
+			Start root = p.parse();
+			RulesGenerator gen = new RulesGenerator();
+			root.apply(gen);
+			return gen.getTokenizers();
+		} catch (LexerException ex) {
+			throw new RuntimeException(ex);
+
+		} catch (ParserException ex) {
+			throw new RuntimeException(ex);
+
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+}
+
+class RulesGenerator extends DepthFirstAdapter {
+	private List<Tokenizer> tokenizers = Lists.newLinkedList();
+	
+	@Override
+	public void caseATokenDeclaration(ATokenDeclaration node) {
+		String name = node.getIdentifier().getText();
+		String regex = stripQuotes(node.getStringLiteral().getText());
+		TokenType type = TokenType.getInstance(name);
+		tokenizers.add(new RegexTokenizer(type, regex));
+	}
+	
+	private String stripQuotes(String s)
+	{
+		return s.substring(1, s.length()-1);
+	}
+	public List<Tokenizer> getTokenizers() {
+		return tokenizers;
+	}
+}
---
+++ trunk/src/main/java/net/sf/textbeans/internal/grammar/Token.java
@@ -0,0 +1,36 @@
+package net.sf.textbeans.internal.grammar;
+
+import java.util.Comparator;
+
+public class Token {
+	private final TokenType type;
+	private final String text;
+	private final int startInd;
+	
+	public static final Comparator<Token> START_POS_ASCENDING = new Comparator<Token>(){
+		@Override
+		public int compare(Token o1, Token o2) {
+			return o1.getStartInd() - o2.getStartInd();
+		}
+	};
+	
+	public Token(TokenType type, String text, int startInd) {
+		super();
+		this.type = type;
+		this.text = text;
+		this.startInd = startInd;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+	public int getStartInd() {
+		return startInd;
+	}
+	
+	public TokenType getType() {
+		return type;
+	}
+
+}
