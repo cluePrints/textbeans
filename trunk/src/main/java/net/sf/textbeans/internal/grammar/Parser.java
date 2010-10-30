abe6ef2f409b0bb9b4a2da09db18ec23b5e3d4d5
some.unique.login@gmail.com <some.unique.login@gmail.com@0e4c521a-48eb-3509-bcc3-8a98805872c0>
2010-10-30 14:36:46 +0000

    Remove unused rubbish

--- trunk/src/main/java/net/sf/textbeans/internal/grammar/Parser.java
+++
@@ -1,5 +0,0 @@
-package net.sf.textbeans.internal.grammar;
-
-public class Parser {
-
-}
--- trunk/src/main/java/net/sf/textbeans/internal/grammar/Tokenizer.java
+++
@@ -1,9 +0,0 @@
-package net.sf.textbeans.internal.grammar;
-
-import java.util.Collection;
-
-public interface Tokenizer {
-
-	public abstract Collection<Token> tokenize(String wholeText);
-	String getName();
-}
