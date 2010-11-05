4ace7ea2757b2c690329175336d1096a86dd90c3
some.unique.login@gmail.com <some.unique.login@gmail.com@0e4c521a-48eb-3509-bcc3-8a98805872c0>
2010-11-05 17:04:28 +0000

    Rearrange classes and check wiring

---
+++ trunk/src/main/java/net/sf/textbeans/binding/BindingListener.java
@@ -0,0 +1,5 @@
+package net.sf.textbeans.binding;
+
+public class BindingListener {
+
+}
--- trunk/src/test/java/net/sf/textbeans/parser/BindingParsingTest.java
+++ trunk/src/test/java/net/sf/textbeans/parser/BindingParsingTest.java
@@ -1,5 +1,7 @@
 package net.sf.textbeans.parser;
 
+import java.io.File;
+
 public class BindingParsingTest extends TatooTest {
 	
 	public BindingParsingTest(String name) {
@@ -9,7 +11,11 @@ public class BindingParsingTest extends TatooTest {
 	@Override
 	protected void compile(String dir, String name) throws Exception {
 		String grammarFile = dir + name + ".ebnf";
+		String rulesFile = dir + name + ".xml";
 		TextBindingParser p = new TextBindingParser().compile(grammarFile);
+		if (new File(rulesFile).exists()) {
+			p.loadAstRules(rulesFile);
+		}		
 		String dataFile = dir + name + ".txt";
 		p.parse(dataFile);
 	}
