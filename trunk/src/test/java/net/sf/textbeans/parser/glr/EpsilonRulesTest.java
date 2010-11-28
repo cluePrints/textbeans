d76153a8dc07ed3d2ee1b07268fb94fe30780bd1
some.unique.login@gmail.com <some.unique.login@gmail.com@0e4c521a-48eb-3509-bcc3-8a98805872c0>
2010-11-28 23:26:53 +0000

    Add a test case with epsilon rules cycling

---
+++ trunk/src/test/java/net/sf/textbeans/parser/glr/EpsilonRulesTest.java
@@ -0,0 +1,30 @@
+package net.sf.textbeans.parser.glr;
+
+import java.io.File;
+import java.io.FileReader;
+
+import net.sf.textbeans.parser.BindingParser;
+import net.sf.textbeans.parser.TatooTest;
+import net.sf.textbeans.util.Const;
+
+import org.junit.Test;
+
+public class EpsilonRulesTest{
+	final String name = "EpsilonRules";
+	final String dir = TatooTest.TEST_DIR+"/parser/glr/";
+	
+	@Test
+	public void shouldReturnProperResult() throws Exception
+	{
+		String grammarFile = dir + name + Const.EBNF_EXT;
+		String rulesFile = dir + name + Const.BINDING_EXT;
+		BindingParser p = new BindingParser().compile(new FileReader(
+				grammarFile));
+		if (new File(rulesFile).exists()) {
+			p.loadAstRules(new FileReader(rulesFile));
+		}
+		String dataFile = dir + name + Const.TEST_CASE_EXT;
+		
+		p.parse(new FileReader(dataFile));		
+	}
+}
